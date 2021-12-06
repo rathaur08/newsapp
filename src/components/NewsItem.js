095bfb7a52c4a98288f8e2c1e66049ed91accffb
Sunny Rathaur <83154334+sunnyrathaur2444@users.noreply.github.com>
2021-12-06 19:54:27 +0530

    Hlo

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
         let {title, description, imageUrl, newsUrl} = this.props;
         return (
             <div className="my-3">
-                <div className="card" style={{width: "18rem"}}>
+                <div className="card" >
                     <img src={!imageUrl ?"https://www.foxnews.com/politics/harris-denounces-european-explorers-for-ushering-in-wave-of-devastation":imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
